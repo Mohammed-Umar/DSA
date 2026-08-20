0dda7766d635381249aebee0a471f9d921251782
Mohammed Umar <104309903+Mohammed-Umar@users.noreply.github.com>
2026-08-20 10:15:54 +0530

    Time: 12 ms (28.90%) | Memory: 19.3 MB (54.84%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x_str = str(x)
+        copy_str = ""
+        for digit in x_str:
+            if not digit.isnumeric():
+                print('Sign encountered.')
+                return False
+
+            else:
+                copy_str += digit
+
+        if copy_str == x_str[::-1]:
+            return True
+        else:
+            return False
