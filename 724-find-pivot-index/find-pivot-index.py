9797e91f23f71f639d327e7c0e4d942b86663afc
Mohammed Umar <104309903+Mohammed-Umar@users.noreply.github.com>
2026-09-07 14:54:08 +0530

    Time: 3 ms (89.28%) | Memory: 20.4 MB (23.88%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,33 @@
+# class Solution:
+
+#   def pivotIndex(self, nums: List[int]) -> int:
+    # i = 0
+    # while i < len(nums):
+    #   left_sum = 0
+    #   right_sum = 0
+
+    #   for j in range(0, i):
+    #     left_sum += nums[j]
+
+    #   for k in range(i + 1, len(nums)):
+    #     right_sum += nums[k]
+
+    #   if left_sum == right_sum:
+    #     return i
+
+    #   i += 1
+
+    # return -1
+class Solution:
+
+  def pivotIndex(self, nums: List[int]) -> int:
+    total_sum = sum(nums)
+    left_sum = 0
+
+    for i, num in enumerate(nums):
+      # Right sum is total_sum - left_sum - current element
+      if left_sum == total_sum - left_sum - num:
+        return i
+      left_sum += num
+
+    return -1
